9cb05db57c779a38e30ec367fe8cfd44a215de2d
SardarBadar <dataentrybadar@gmail.com>
2022-07-13 15:06:25 +0500

    Updates

---
+++ precache-manifest.98a2e7bccdb5cc779b9a700447bba48c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a5d5046e742eeda3f6",
+    "url": "/netlifyitm/static/css/main.89ab352b.chunk.css"
+  },
+  {
+    "revision": "12a5d5046e742eeda3f6",
+    "url": "/netlifyitm/static/js/main.12a5d504.chunk.js"
+  },
+  {
+    "revision": "ad17cc3b6af54af3d21f",
+    "url": "/netlifyitm/static/js/runtime~main.ad17cc3b.js"
+  },
+  {
+    "revision": "878b1ac8d7a964040886",
+    "url": "/netlifyitm/static/css/2.243bbba2.chunk.css"
+  },
+  {
+    "revision": "878b1ac8d7a964040886",
+    "url": "/netlifyitm/static/js/2.878b1ac8.chunk.js"
+  },
+  {
+    "revision": "4e59f7c5f6275ec64349f95e8e2bdf29",
+    "url": "/netlifyitm/static/media/18.4e59f7c5.jpg"
+  },
+  {
+    "revision": "ab3ade4099cd9d1e60edd9979e0e35f6",
+    "url": "/netlifyitm/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/netlifyitm/precache-manifest.98a2e7bccdb5cc779b9a700447bba48c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/netlifyitm/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
